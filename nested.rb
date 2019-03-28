6383a562be9b365ef16071147ecf0fb46a8d3deb
Spencer Lindemuth <lindemut@gmail.com>
2019-03-28 20:36:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,5 +127,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+		 add = programmer_hash[:dennis_ritchie][:languages]
+		 add.push("Assembly")
+		 programmer_hash[:dennis_ritchie][:languages] = add
+		 programmer_hash
 end
